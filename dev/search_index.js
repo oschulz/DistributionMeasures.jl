0f610d610cde07df5fd184a7c6a7fe9ef5f06e4e
Documenter.jl <documenter@juliadocs.github.io>
2022-05-31 22:42:53 +0000

    build based on 45e7d52

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DistributionMeasures","category":"page"},{"location":"#DistributionMeasures","page":"Home","title":"DistributionMeasures","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DistributionMeasures.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DistributionMeasures]","category":"page"}]
+}
