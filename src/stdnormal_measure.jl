3bc90408fd1c55b0f885233f9515e7c355518197
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-16 16:51:30 +0200

    Fixup StdNormal measure

--- src/stdnormal_measure.jl
+++ src/stdnormal_measure.jl
@@ -2,11 +2,7 @@ struct StdNormal <: MeasureBase.StdMeasure end
 
 export StdNormal
 
-@inline _isreal(x) = False
-@inline _isreal(x::Real) = True
-
-@inline MeasureBase.insupport(d::StdNormal, x) = _isreal(x)
-@inline MeasureBase.insupport(d::StdNormal) = _isreal
+@inline MeasureBase.insupport(d::StdNormal, x) = true
 
 @inline MeasureBase.logdensity_def(::StdNormal, x) = -x^2 / 2
 @inline MeasureBase.basemeasure(::StdNormal) = WeightedMeasure(static(-0.5 * log2π), Lebesgue(ℝ))
