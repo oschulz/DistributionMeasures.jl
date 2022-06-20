f68e214d495dd664363627bcbeab27e975e2fe3f
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-20 10:17:38 -0400

    STASH tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @testset "DistributionMeasures.jl" begin
     include("test_autodiff_utils.jl")
     include("test_measure_interface.jl")
+    include("test_distribution_measure.jl")
     include("test_standard_dist.jl")
     include("test_standard_uniform.jl")
     include("test_standard_normal.jl")
