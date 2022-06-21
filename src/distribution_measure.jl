03c5f17f332de30c75416cab9bae8c1d52964c8a
Oliver Schulz <oschulz@mpp.mpg.de>
2022-06-21 03:10:27 -0400

    Fully implement DistributionMeasure and add var transforms (#6)

--- src/distribution_measure.jl
+++ src/distribution_measure.jl
@@ -1,3 +1,6 @@
+# This file is a part of DistributionMeasures.jl, licensed under the MIT License (MIT).
+
+
 """
     struct DistributionMeasure <: AbstractMeasure
 
@@ -14,11 +17,12 @@ struct DistributionMeasure{F<:VariateForm,S<:ValueSupport,D<:Distribution{F,S}} 
     d::D
 end
 
+
 @inline MeasureBase.AbstractMeasure(d::Distribution) = DistributionMeasure(d)
 
 @inline Base.convert(::Type{AbstractMeasure}, d::Distribution) = DistributionMeasure(d)
 
-@inline Distributions.Distribution(m::DistributionMeasure) = m.distribution
+@inline Distributions.Distribution(m::DistributionMeasure) = m.d
 @inline Distributions.Distribution{F}(m::DistributionMeasure{F}) where {F<:VariateForm} = Distribution(m)
 @inline Distributions.Distribution{F,S}(m::DistributionMeasure{F,S}) where {F<:VariateForm,S<:ValueSupport} = Distribution(m)
 
@@ -26,40 +30,39 @@ end
 @inline Base.convert(::Type{Distribution{F}}, m::DistributionMeasure{F}) where {F<:VariateForm} = Distribution(m)
 @inline Base.convert(::Type{Distribution{F,S}}, m::DistributionMeasure{F,S}) where {F<:VariateForm,S<:ValueSupport} = Distribution(m)
 
-@inline DensityInterface.densityof(m::DistributionMeasure) = DensityInterface.densityof(m.d)
-@inline DensityInterface.densityof(m::DistributionMeasure, x) = DensityInterface.densityof(m.d, x)
-@inline DensityInterface.logdensityof(m::DistributionMeasure) = DensityInterface.logdensityof(m.d)
-@inline DensityInterface.logdensityof(m::DistributionMeasure, x) = DensityInterface.logdensityof(m.d, x)
 
-@inline MeasureBase.logdensity_def(m::DistributionMeasure, x) = DensityInterface.logdensityof(m.d, x)
-@inline MeasureBase.unsafe_logdensityof(m::DistributionMeasure, x) = DensityInterface.logdensityof(m.d, x)
+@inline DensityInterface.densityof(μ::DistributionMeasure) = DensityInterface.densityof(μ.d)
+@inline DensityInterface.densityof(μ::DistributionMeasure, x) = DensityInterface.densityof(μ.d, x)
+@inline DensityInterface.logdensityof(μ::DistributionMeasure) = DensityInterface.logdensityof(μ.d)
+@inline DensityInterface.logdensityof(μ::DistributionMeasure, x) = DensityInterface.logdensityof(μ.d, x)
 
-@inline MeasureBase.insupport(m::DistributionMeasure, x) = Distributions.insupport(m.x)
+@inline MeasureBase.logdensity_def(μ::DistributionMeasure, x) = MeasureBase.logdensity_def(μ.d, x)
+@inline MeasureBase.unsafe_logdensityof(μ::DistributionMeasure, x) = MeasureBase.unsafe_logdensityof(μ.d, x)
+@inline MeasureBase.insupport(μ::DistributionMeasure, x) = MeasureBase.insupport(μ.d, x)
+@inline MeasureBase.basemeasure(μ::DistributionMeasure) = MeasureBase.basemeasure(μ.d)
+@inline MeasureBase.paramnames(μ::DistributionMeasure) = MeasureBase.paramnames(μ.d)
+@inline MeasureBase.params(μ::DistributionMeasure) = MeasureBase.params(μ.d)
+@inline MeasureBase.transport_origin(ν::DistributionMeasure) = ν.d
+@inline MeasureBase.to_origin(::DistributionMeasure, y) = y
+@inline MeasureBase.from_origin(::DistributionMeasure, x) = x
 
-@inline MeasureBase.basemeasure(m::DistributionMeasure{<:ArrayLikeVariate{0},<:Continuous}) = Lebesgue()
-@inline MeasureBase.basemeasure(m::DistributionMeasure{<:ArrayLikeVariate,<:Continuous}) = Lebesgue()^size(m.d)
-@inline MeasureBase.basemeasure(m::DistributionMeasure{<:ArrayLikeVariate{0},<:Discrete}) = Counting()
-@inline MeasureBase.basemeasure(m::DistributionMeasure{<:ArrayLikeVariate,<:Discrete}) = Counting()^size(m.d)
 
-@inline MeasureBase.rootmeasure(m::DistributionMeasure) = MeasureBase.basemeasure(m)
-
-
-Base.rand(rng::AbstractRNG, ::Type{T}, m::DistributionMeasure) where {T<:Real} = _convert_numtype(T, rand(m.d))
+Base.rand(rng::AbstractRNG, ::Type{T}, m::DistributionMeasure) where {T<:Real} = convert_realtype(T, rand(m.d))
 
 function _flat_powrand(rng::AbstractRNG, ::Type{T}, d::Distribution{<:ArrayLikeVariate{0}}, sz::Dims) where {T<:Real}
-    _convert_numtype(T, reshape(rand(d, prod(sz)), sz...))
+    convert_realtype(T, reshape(rand(d, prod(sz)), sz...))
 end
 
 function _flat_powrand(rng::AbstractRNG, ::Type{T}, d::Distribution{<:ArrayLikeVariate{1}}, sz::Dims) where {T<:Real}
-    _convert_numtype(T, reshape(rand(d, prod(sz)), size(d)..., sz...))
+    convert_realtype(T, reshape(rand(d, prod(sz)), size(d)..., sz...))
 end
 
 function _flat_powrand(rng::AbstractRNG, ::Type{T}, d::ReshapedDistribution{N,<:Any,<:Distribution{<:ArrayLikeVariate{1}}}, sz::Dims) where {T<:Real,N}
-    _convert_numtype(T, reshape(rand(d.dist, prod(sz)), d.dims..., sz...))
+    convert_realtype(T, reshape(rand(d.dist, prod(sz)), d.dims..., sz...))
 end
 
 function _flat_powrand(rng::AbstractRNG, ::Type{T}, d::Distribution, sz::Dims) where {T<:Real,N}
-    flatview(ArrayOfSimilarArrays(_convert_numtype(T, rand(d, sz))))
+    flatview(ArrayOfSimilarArrays(convert_realtype(T, rand(d, sz))))
 end
 
 function Base.rand(rng::AbstractRNG, ::Type{T}, m::PowerMeasure{<:DistributionMeasure{<:ArrayLikeVariate{0}}, NTuple{N,Base.OneTo{Int}}}) where {T<:Real,N}
@@ -70,7 +73,3 @@ function Base.rand(rng::AbstractRNG, ::Type{T}, m::PowerMeasure{<:DistributionMe
     flat_data = _flat_powrand(rng, T, m.parent.d, map(length, m.axes))
     ArrayOfSimilarArrays{T,M,N}(flat_data)
 end
-
-
-@inline MeasureBase.paramnames(m::DistributionMeasure) = propertynames(m.d)
-@inline MeasureBase.params(m::DistributionMeasure) = NamedTuple{MeasureBase.paramnames(m.d)}(Distributions.params(m.d))
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using DistributionMeasures
 using Test
 
 @testset "DistributionMeasures.jl" begin
-    # Write your tests here.
+    include("test_autodiff_utils.jl")
+    include("test_measure_interface.jl")
+    include("test_distribution_measure.jl")
+    include("test_standard_dist.jl")
+    include("test_standard_uniform.jl")
+    include("test_standard_normal.jl")
+    include("test_transport.jl")
 end
